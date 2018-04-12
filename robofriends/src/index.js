38cad6f3506c5fe981b91f6cb747d9a8e575f951
co-deion <cdphicks.0@gmail.com>
2018-04-12 18:11:02 -0500

    update to working code

--- robofriends/src/index.js
+++ robofriends/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 import 'tachyons';
-import CardList from './Cardlist'
+import CardList from './CardList'
 import { Robots } from './Robots';
 
 ReactDOM.render(<CardList Robots={Robots}/>, document.getElementById('root'));
